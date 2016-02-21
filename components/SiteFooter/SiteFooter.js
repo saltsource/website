c0795edd362be6575448c08accad798e3610df0b
Jared Anderson <jared@tuxsudo.com>
2016-02-20 17:47:44 -0700

    Silly style tweaks.
    Move Mr. Salt to header. Added copyright. Basically, just bored and
    fooling around until real styles...

--- components/SiteFooter/SiteFooter.js
+++ components/SiteFooter/SiteFooter.js
@@ -1,9 +1,8 @@
 import styles from './SiteFooter.css';
 import classNames from 'classnames';
-import img from './bcsalt.gif';
 
 export default ({className}) => (
     <footer className={classNames(styles.footer, className)}>
-        <img className={styles.img} src={img} />
+        Copyright (c) 2016 SaltSource All Rights Reserved.
     </footer>
 );
--- components/SiteHeader/SiteHeader.js
+++ components/SiteHeader/SiteHeader.js
@@ -2,10 +2,13 @@ import style from './SiteHeader.css';
 import SiteNav from '../SiteNav/SiteNav.js';
 import classNames from 'classnames';
 import { IndexLink } from 'react-router';
+import img from "./bcsalt.gif";
 
 export default ({className=""}) => (
     <header className={classNames(style.header, className)}>
-        <IndexLink to="/" className={style.brand} activeClassName={style['brand--active']}>Salt Source</IndexLink>
+        <IndexLink to="/" className={style.brand} activeClassName={style['brand--active']}>
+            <img className={style.img} src={img} alt="SaltSource" />
+        </IndexLink>
         <SiteNav className={style.nav} />
     </header>
 );
